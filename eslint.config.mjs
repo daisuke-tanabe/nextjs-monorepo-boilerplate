a4c3dbf290f308ea670a22b96a6552e6f5b430e5
daisuke-tanabe <dev.daisuke.tanabe@gmail.com>
2024-04-13 13:21:54 +0900

    chore: setup eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,41 @@
+import eslint from '@eslint/js';
+import { FlatCompat } from '@eslint/eslintrc';
+import tseslint from 'typescript-eslint';
+
+const compat = new FlatCompat();
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: [
+          './apps/web/tsconfig.json',
+        ],
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+  {
+    files: [
+      "apps/web/**/*.[jt]s",
+      "apps/web/**/*.[cm]js",
+      "apps/web/**/*.[cm]ts",
+      "apps/web/**/*.[jt]sx",
+    ],
+    extends: compat.extends('next/core-web-vitals'),
+    settings: {
+      // Locating the Next.js application
+      // https://nextjs.org/docs/app/building-your-application/configuring/eslint#custom-settings
+      next: {
+        rootDir: 'apps/web'
+      }
+    },
+  },
+  {
+    files: ['**/*.js', '**/*.[cm]js', '**/*.jsx'],
+    extends: [tseslint.configs.disableTypeChecked],
+  },
+);
